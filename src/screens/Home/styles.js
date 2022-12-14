54a83742a73d18d6d2999e7dd60babe815728dcf
asrulabdullah99 <srulabdullah@gmail.com>
2022-12-15 00:05:16 +0700

    not fixed screen di pengumuman

--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -3,7 +3,7 @@ import {colors} from '../../theme'
 
 const Styles = StyleSheet.create({
   container: {
-    marginTop: 46,
+    marginTop: 60,
     marginBottom: 22,
     marginHorizontal: 22,
     height: '100%',
